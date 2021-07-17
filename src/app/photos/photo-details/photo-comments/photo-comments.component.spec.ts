6ebc049c14e6a9e02d1dd1eaf14e3a8210e09919
Erik <erik@beapartof.com>
2021-07-16 22:14:27 -0400

    ng update

--- src/app/photos/photo-details/photo-comments/photo-comments.component.spec.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PhotoCommentsComponent } from './photo-comments.component';
 
@@ -6,7 +6,7 @@ describe('PhotoCommentsComponent', () => {
   let component: PhotoCommentsComponent;
   let fixture: ComponentFixture<PhotoCommentsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PhotoCommentsComponent ]
     })
